bf49137560133e1e0cb7a5f8b8a84978c630ac1c
Ezio1997-K <2911019397@qq.com>
2024-12-22 22:28:41 +0800

    初始化

---
+++ src/test/java/com/bootzero/big_event/BigEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootzero.big_event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigEventApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
