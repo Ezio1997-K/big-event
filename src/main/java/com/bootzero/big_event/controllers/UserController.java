53ccf080f3447dedf6f8df81a255f2bc895e7d26
Ezio1997-K <2911019397@qq.com>
2024-12-25 19:04:30 +0800

    实现登录功能

--- src/main/java/com/bootzero/big_event/controllers/UserController.java
+++ src/main/java/com/bootzero/big_event/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.bootzero.big_event.bean.User;
 import com.bootzero.big_event.service.UserService;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,5 +34,18 @@ public class UserController {
         }
     }
 
+    @PostMapping("/login")
+    public Result<?> login(@Pattern(regexp = "\\S{5,16}") String username,@Pattern(regexp = "\\S{5,16}") String password) {
+        //判断用户是否已经存在
+        User user = userService.findUserByName(username);
+        if (user == null) {
+            return Result.<Void>error("该用户不存在！");
+        }
 
+        if(DigestUtils.md5DigestAsHex(password.getBytes()).equals(user.getPassword())){
+            return Result.success("jwt令牌");
+        }else {
+            return Result.<Void>error("密码错误");
+        }
+    }
 }
