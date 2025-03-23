e1ae23155159af1d8b5a18c9e5791ca901a5b368
Ezio1997-K <2911019397@qq.com>
2025-03-23 18:16:03 +0800

    解决跨域问题

--- src/main/java/com/bootzero/big_event/config/WebConfig.java
+++ src/main/java/com/bootzero/big_event/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.bootzero.big_event.interceptors.LoginInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,5 +20,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginInterceptor).excludePathPatterns("/user/login","/user/register");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173","http://127.0.0.1:5173")
+                .allowCredentials(true)
+                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                .maxAge(3600);
     }
 }
