b23b92824f6d3800d359d1162168ec8956ff74db
Ezio1997-K <2911019397@qq.com>
2025-04-14 12:12:33 +0800

    新增私有方法判断桶是否存在

--- src/main/java/com/bootzero/big_event/service/MinioService.java
+++ src/main/java/com/bootzero/big_event/service/MinioService.java
@@ -1,4 +1,37 @@
 package com.bootzero.big_event.service;
 
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
+import io.minio.errors.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class MinioService {
+
+    private final MinioClient minioClient;
+    @Value("${minio.bucket-name}")
+    private String bucketName;
+
+    /**
+     * 检查存储桶是否存在，不存在则创建
+     */
+    private void ensureBucketExists() throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        boolean found = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
+        if(!found){
+            minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+            log.info("{}Bucket Created successfully", bucketName);
+        }else {
+            log.info("{}Bucket already exists", bucketName);
+        }
+    }
 }
