09622d1afa50842130f82fac1e76d1317a9f4309
Ezio1997-K <2911019397@qq.com>
2025-04-14 15:10:22 +0800

    上传文件

--- src/main/java/com/bootzero/big_event/service/MinioService.java
+++ src/main/java/com/bootzero/big_event/service/MinioService.java
@@ -3,13 +3,16 @@ package com.bootzero.big_event.service;
 import io.minio.BucketExistsArgs;
 import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
 import io.minio.errors.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
@@ -32,6 +35,35 @@ public class MinioService {
             log.info("{}Bucket Created successfully", bucketName);
         }else {
             log.info("{}Bucket already exists", bucketName);
+        }
+    }
+
+    /**
+     * 上传文件
+     *
+     * @param file        MultipartFile 文件对象
+     * @param objectName  存储在 MinIO 中的对象名称 (可以包含路径，例如 "images/myphoto.jpg")
+     *                    如果不指定，可以使用 file.getOriginalFilename()
+     * @return 返回存储的对象名称
+     */
+    public String uploadFile(MultipartFile file,String objectName) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        //确保桶存在
+        ensureBucketExists();
+        if (objectName == null||objectName.isEmpty()) {
+            objectName = file.getOriginalFilename();
+        }
+        try(InputStream inputStream = file.getInputStream()){
+            minioClient.putObject(PutObjectArgs.builder()
+                    .bucket(bucketName)
+                    .object(objectName)
+                    .stream(inputStream,file.getSize(),-1)// -1 表示未知部分大小，让 MinIO 自动处理
+                    .contentType(file.getContentType())//设置content-type
+                    .build());
+            log.info("文件{}成功上传到bucket{}",objectName,bucketName);
+            return objectName;//返回对象名
+        } catch (Exception e) {
+            log.error("文件上传发生错误：{}",e.getMessage());
+            throw e;//由Controller处理
         }
     }
 }
